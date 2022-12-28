b66589e8b80423a39fe69698b946e7985fbc5877
RobertKeanu <rosianu.robert@yahoo.com>
2022-12-28 14:42:54 +0200

    feat/read

--- src/api.c
+++ src/api.c
@@ -13,13 +13,9 @@ void* btrdt_init(struct fuse_conn_info *conn, struct fuse_config *cfg){
     btrdt_data* fs_data = fuse_get_context()->private_data;
 
     fs_data->root = new_node();
-    fs_data->container = archive_read_new();
-
-    archive_read_support_format_tar(fs_data->container);
 
     //we build the directory structure
     build_tree(fs_data->root,
-              fs_data->container,
               fs_data->archive_fd,
               &fs_data->mount_st);
     
@@ -85,74 +81,48 @@ int btrdt_readdir(const char* path, void* buffer, fuse_fill_dir_t filler, off_t 
     
     return 0;
 }
-int btrdt_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
-{
+int btrdt_read(const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi ){
     btrdt_data* fs_data = fuse_get_context()->private_data;
     node* found = find_node(fs_data->root,path);
-    int ret = -1;
-    if(found == NULL)
-    {
+    int read_size = -ENOENT;
+    const char* realpath;
+    struct archive_entry *entry;
+
+    if(found == NULL){
         return -ENOENT;
     }
-    struct stat* st;
-    node* nod;
-    const char* realpath;
-    if(found->modificat)
-    {
-        int g = open(found->path,O_RDONLY);
-        if(g == -1)
-        {
-            return 0 - errno;
-        }
-        if((ret = pread(g,buffer,size,offset))==-1)
-        {
-            close(g);
-            return 0 - errno;
-        }
-        close(g);
-    }
-    else
-    {
-        struct archive *arc;
-        struct archive *arc2 = archive_read_new();
-        struct archive_entry *entry;
-        int ret2;
-        int arc_ret;
-        realpath = archive_entry_pathname(found->entry);
-        if (arc2 == NULL)
-        {
-			return -ENOMEM;
-		}
-        ret2 = archive_read_open_fd(arc2,archive_fd,BLOCK_SIZE);
-        if(ret2 != ARCHIVE_OK)
-            return -EIO;
-        while((ret2 = archive_read_next_header(arc2,&entry))==ARCHIVE_OK)
-        {
-            const char* name;
-            name = archive_entry_pathname(entry);
-            if(strcmp(realpath,name) == 0)
-            {
-                void* trash;
-                while(offset)
-                {
-                    int skip;
-                    if(offset > 4096)
-                        skip = offset;
-                    else
-                        skip = 4096;
-                    ret = archive_read_data(arc2,trash,skip);
-                    offset = offset - skip;
-                }   
-                free(trash);
-                if(offset)
-                    break;
-                ret = archive_read_data(arc2,buffer,size);
-                break;
+    realpath = archive_entry_pathname(found->entry);
+    struct archive *arc = archive_read_new();
+
+    if(archive_read_open_fd(arc,fs_data->archive_fd,BLOCK_SIZE) != ARCHIVE_OK)
+        return -archive_errno(arc);
+    
+    while(archive_read_next_header(arc,&entry)==ARCHIVE_OK){
+        const char* path;
+        path = archive_entry_pathname(entry);
+        if(strcmp(realpath,path) == 0){
+            if(offset + size > archive_entry_size(entry)){
+                return 0;
             }
-            archive_read_data_skip(arc2);
+            void* trash = malloc(IO_BLOCK);
+            while(offset){
+                int skip;
+                if(offset > IO_BLOCK)
+                    skip = offset;
+                else
+                    skip = IO_BLOCK;
+                archive_read_data(arc,trash,skip);
+                //warnings
+                offset = offset - skip;
+            }   
+            free(trash);
+            read_size = archive_read_data(arc,buffer,size);
+            //warnings
+            break;
         }
-        archive_read_free(arc2);
-        lseek(archive_fd, 0, SEEK_SET);
+        archive_read_data_skip(arc);
     }
-    return ret;
+    archive_read_free(arc);
+    lseek(fs_data->archive_fd, 0, SEEK_SET);
+    return read_size;
 }
